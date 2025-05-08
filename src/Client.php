48b910c9db644a0acc8355fe532f02921a2d45f0
Golo Roden <golo.roden@thenativeweb.io>
2025-05-08 23:42:44 +0200

    Implement ping function.

---
+++ src/Client.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Thenativeweb\Eventsourcingdb;
+
+use GuzzleHttp\Client as HttpClient;
+use GuzzleHttp\Exception\GuzzleException;
+use RuntimeException;
+
+class Client
+{
+	private string $apiToken;
+	private HttpClient $httpClient;
+
+	public function __construct(string $url, string $apiToken)
+	{
+		$this->apiToken = $apiToken;
+		$this->httpClient = new HttpClient([
+			'base_uri' => rtrim($url, '/'),
+			'http_errors' => false,
+		]);
+	}
+
+	public function ping(): void
+	{
+		$response = $this->httpClient->get('/api/v1/ping');
+		$status = $response->getStatusCode();
+
+		if ($status !== 200) {
+			throw new RuntimeException(sprintf(
+				"Failed to ping, got HTTP status code '%d', expected '200'",
+				$status
+			));
+		}
+
+		$body = (string) $response->getBody();
+		$data = json_decode($body, true);
+
+		if (!isset($data['type']) || $data['type'] !== 'io.eventsourcingdb.api.ping-received') {
+			throw new RuntimeException('Failed to ping');
+		}
+	}
+}
