b9464278c213174f7ce390721344e3c7a01b070b
awu <andreas.wunderwald@westpress.de>
2025-07-22 13:34:04 +0200

    refactor: change Header class to final and readonly for improved immutability

--- src/Stream/Header.php
+++ src/Stream/Header.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace Thenativeweb\Eventsourcingdb\Stream;
 
-class Header
+final readonly class Header
 {
     public function __construct(
-        public readonly int $statusCode,
-        public readonly string $httpVersion,
-        public readonly string $contentType,
-        public readonly int $contentLength,
+        public int $statusCode,
+        public string $httpVersion,
+        public string $contentType,
+        public int $contentLength,
     ) {
     }
 }
